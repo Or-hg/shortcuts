f2eb9597435dcc1ce3090d7e6fdb412e27aaa931
or <or.hupert.graff1@gmail.com>
2023-08-27 13:11:30 +0300

    Add Window mgr

--- src/shortcuts/__init__.py
+++ src/shortcuts/__init__.py
@@ -2,3 +2,4 @@ from .expression import Expression
 from .terminal_expression import TerminalExpression
 from .change_volume import ChangeVolume
 from .app_focus import AppFocus
+from .window_mgr import WindowMgr
--- tests/shortcuts_tests/test_app_focus.py
+++ tests/shortcuts_tests/test_app_focus.py
@@ -1,6 +1,7 @@
 from shortcuts import AppFocus, ChangeVolume, TerminalExpression
 
 
-def test_app_focus():
+def test_app_focus_change_volume():
     AppFocus("spotify", ChangeVolume(TerminalExpression(50))).execute()
+
 
